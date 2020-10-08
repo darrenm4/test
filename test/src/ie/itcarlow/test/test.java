3807a4bf7e57415b412717f3987d86ae4c47ca7c
darrenm4 <72545648+darrenm4@users.noreply.github.com>
2020-10-08 11:22:29 +0100

    added a second statement

--- test/src/ie/itcarlow/test/test.java
+++ test/src/ie/itcarlow/test/test.java
@@ -5,7 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is a test");
-
+		System.out.println("added a second statement");
 	}
 
 }
