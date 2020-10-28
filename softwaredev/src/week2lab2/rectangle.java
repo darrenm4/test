c0e30d2929704c7b6ef415fc1b41801a7b62b904
Darren <Darren@LAPTOP-M4B30GVV>
2020-10-28 10:49:11 +0000

    exercise 1 (rectangle)

---
+++ softwaredev/src/week2lab2/rectangle.java
@@ -0,0 +1,43 @@
+package week2lab2;
+
+public class rectangle {
+	
+	//Variables//
+	private float length;
+	private float width;
+	
+	
+	//Constructors//
+	public rectangle() {
+		setLength(1.0f);
+		setWidth(1.0f);
+	}
+	
+	//Getters & Setters//
+	public float getLength() {
+		return length;
+	}
+	
+	public void setLength(float length) {
+		if(length>0.0 && length<=40.0) {
+			this.length = length;
+		}
+		else {
+			System.out.println("length must be between 0.0 and 40.0");
+		}
+	}
+	public float getWidth() {
+		return width;
+	}
+	public void setWidth(float width) {
+		if(width>0.0 && width <= 40.0) {
+		this.width = width;
+		}
+		else {
+			System.out.println("width must be between 0.0 and 40.0");
+		}
+	}
+	
+	//ToString//
+
+}
