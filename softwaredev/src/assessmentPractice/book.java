ddcda90017e60dcc578ab3c24e0bde88eed5cf8e
Darren <Darren@LAPTOP-M4B30GVV>
2020-11-03 11:45:06 +0000

    assessment prractice

---
+++ softwaredev/src/assessmentPractice/book.java
@@ -0,0 +1,73 @@
+package assessmentPractice;
+
+public class book {
+	
+	//Varibales 
+	private String name;
+	private String author;
+	private int ISBN;
+	private String genre;
+	private char status;
+	private static int noOfBooks=0;
+	private static int nextISBN = 1;
+	
+	//Consructors
+	public book(String name, String author, String genre) {
+		super();
+		setName(name);
+		setAuthor(author);
+		setGenre(genre);
+		setStatus('A');
+		setISBN(nextISBN);
+		nextISBN ++;
+		noOfBooks++;
+	}
+	
+	//Getters & Setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getISBN() {
+		return ISBN;
+	}
+	public void setISBN(int iSBN) {
+		ISBN = iSBN;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public char getStatus() {
+		return status;
+	}
+	public void setStatus(char status) {
+		this.status = status;
+	}
+	public static int getNoOfBooks() {
+		return noOfBooks;
+	}
+	public static void setNoOfBooks(int noOfBooks) {
+		book.noOfBooks = noOfBooks;
+	}
+	
+	//toString
+	
+	
+	//misc metohds
+	public void Borrow() {
+		setStatus('B');
+	}
+	
+	
+}
---
+++ softwaredev/src/assessmentPractice/library.java
@@ -0,0 +1,10 @@
+package assessmentPractice;
+
+public class library {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
