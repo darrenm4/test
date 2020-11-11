503aa43a9007a5b892471dd9a3325fb73b07f38f
Darren <Darren@LAPTOP-M4B30GVV>
2020-11-11 10:54:45 +0000

    bank customer done

---
+++ softwaredev/src/week4lab1/bankCustomer.java
@@ -0,0 +1,72 @@
+package week4lab1;
+
+import java.util.Arrays;
+
+public class bankCustomer {
+	
+	//variables
+	private String name;
+	private String address;
+	private SavingsAccount[] savaccs = new SavingsAccount[3];
+	private int noOfAccs=0;
+	
+	//constructors
+	public bankCustomer(String name, String address) {
+		setName(name);
+		setAddress(address);
+	}
+	
+	//getters and setters
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public SavingsAccount[] getSavaccs() {
+		return savaccs;
+	}
+	public void setSavaccs(SavingsAccount[] savaccs) {
+		this.savaccs = savaccs;
+	}
+	public int getNoOfAccs() {
+		return noOfAccs;
+	}
+
+	public void setNoOfAccs(int noOfAccs) {
+		this.noOfAccs = noOfAccs;
+	}
+	
+	//misc methods
+
+	public void addAccount(SavingsAccount sav ) {
+		if(getNoOfAccs()<=2) {
+			savaccs[getNoOfAccs()]=sav;
+			setNoOfAccs(getNoOfAccs()+1);
+		}
+		else {
+			System.out.println("You have reached the maximum no of accs");
+		}
+	}
+
+	public double balance(SavingsAccount[]savaccs) {
+		double balance=0;
+		for(SavingsAccount sav: savaccs) {
+			if(sav != null)balance+= sav.getSavingsBalance();
+		}
+		return balance;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return "bankCustomer [name=" + name + ", address=" + address + ", savaccs=" + Arrays.toString(savaccs) + "]";
+	}
+
+}//end main
--- softwaredev/src/week4lab1/bankDriver.java
+++ softwaredev/src/week4lab1/bankDriver.java
@@ -10,16 +10,6 @@ public class bankDriver {
 		
 		SavingsAccount saver2 = new SavingsAccount(3000.00);
 		
-		SavingsAccount.modifyInterestRate(0.04f);
-		
-		saver1.calculateMonthlyInterest();
-		saver2.calculateMonthlyInterest();
-		
-		System.out.println(saver1);
-		System.out.println(saver2.toString());
-		
-		//modify interest rate to 5%
-		
 SavingsAccount.modifyInterestRate(0.05f);
 		
 		saver1.calculateMonthlyInterest();
@@ -27,7 +17,15 @@ SavingsAccount.modifyInterestRate(0.05f);
 		
 		System.out.println(saver1);
 		System.out.println(saver2.toString());
-
+		
+		bankCustomer bc = new bankCustomer("Darren","Kildare");
+		bc.addAccount(saver1);
+		bc.addAccount(saver2);
+		
+		System.out.println(bc);
+		System.out.println("savings total" + bc.balance(bc.getSavaccs()));
+		
+		
 	}//end main
 
 }
