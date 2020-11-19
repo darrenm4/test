82a9f489f86beb7a508f985b86b9b377db718d41
Darren <Darren@LAPTOP-M4B30GVV>
2020-11-19 10:40:04 +0000

    driver alarmTest

--- softwaredev/src/week5lab2/alarmTest.java
+++ softwaredev/src/week5lab2/alarmTest.java
@@ -3,7 +3,17 @@ package week5lab2;
 public class alarmTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		alarmClock myAlarm = new alarmClock(11,0);
+		myAlarm.setalarmTime(11, 1);
+		
+		while(myAlarm.checkAlarmTime()==false) {
+			long wait= System.currentTimeMillis()+1000;
+			while(System.currentTimeMillis()<wait);
+			myAlarm.tick();
+			System.out.println(myAlarm.showCurrentTime());
+			}
+		System.out.println("ring ring");
 
 	}
 
