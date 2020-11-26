07bdfb8c6dac5681152e66ec3c9438995550d5bb
Darren <Darren@LAPTOP-M4B30GVV>
2020-11-26 10:03:42 +0000

    week6lab2

---
+++ softwaredev/src/week6lab2/CarTest.java
@@ -0,0 +1,23 @@
+package week6lab2;
+
+public class CarTest{
+	
+	public static void main(String args[]){
+		
+		Car car1 = new Car();
+		
+		car1.setWheels(4);
+		car1.setPass(6);
+		car1.setType("saloon");
+
+		System.out.println("Car1:" + car1);
+		
+		
+		
+		Car car2=new Car();
+		
+		car2.setWheels(4);
+		car2.setPass(7);
+		car2.setType("people carrier");
+	}
+}
---
+++ softwaredev/src/week6lab2/RoadVehicle.java
@@ -0,0 +1,38 @@
+package week6lab2;
+
+public class RoadVehicle{
+	private int wheels;	    //How many wheels it has
+	private int passengers; //How many passengers it can carry
+ 
+	public RoadVehicle() {
+ 		this(0,0);	
+	}
+ 
+	public RoadVehicle(int w, int p) { 
+		setWheels(w);
+		setPass(p);
+	}
+ 
+	public void setWheels(int num) {
+		wheels = num;
+		}
+ 
+	public int getWheels() {
+		return wheels;
+		}
+ 
+	public void setPass(int num) {
+		passengers = num;
+		}
+ 
+	public int getPass() {
+		return passengers;
+		}
+
+	@Override
+	public String toString() {
+		return "RoadVehicle [wheels=" + wheels + ", passengers=" + passengers + "]";
+	}
+	
+	
+}
